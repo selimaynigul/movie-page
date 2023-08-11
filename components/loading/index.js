f49f3a3ccbe9292335675ac31e20ab8271afa053
Selim Aynigül <98275022+selimaynigul@users.noreply.github.com>
2023-08-11 15:50:38 +0300

    first commit

---
+++ components/loading/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const Loading = () => {
+  return (
+    <div className={styles.loadingWrapper}>
+      <div className={styles.loading}></div>
+    </div>
+  );
+};
+
+export default Loading;
